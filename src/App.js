9d4947d4a3032afb3c205609661c0ec0d0d1b066
wilsonvetdev <wilsongch92@gmail.com>
2021-11-29 17:31:38 -0500

    google maps + rapidapi testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { CssBaseline, Grid  } from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import { getPlacesData } from './api';
 
-function App() {
+const App = () => {
+
+  const [places, setPlaces] = useState([])
+  const [filteredPlaces, setFilteredPlaces] = useState([])
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState(null)
+  const [childClicked, setChildClicked] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
+  const [autocomplete, setAutocomplete] = useState(null)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
+    })
+  }, [])
+
+  useEffect(() => {
+    const filteredPlaces = places?.filter((place) => place.rating > rating)
+    setFilteredPlaces(filteredPlaces)
+  }, [rating])
+
+  useEffect(() => {
+    if(bounds) {
+      setIsLoading(true)
+      getPlacesData(type, bounds.sw, bounds.ne)
+        .then((data) => {
+          setPlaces(data?.filter((place) => place.name && place.num_reviews > 0))
+          setFilteredPlaces([]);
+          setRating('');
+          setIsLoading(false)
+          return () => {setBounds({})}
+        })
+    }
+  }, [bounds, type])
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+
+      <Grid container spacing={3} style={{width: '100%' }}>
+
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+
+      </Grid>
+    </>
   );
 }
 
